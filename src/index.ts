b0a301179b0462bb8470b2f800e6b88ad18e63c1
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-06-03 11:29:39 +0530

    insert data ugly way

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,24 @@
-import dotenv from 'dotenv'
-// write a function to create user table in database
 import { Client } from 'pg';
-
+import dotenv from 'dotenv';
 dotenv.config()
-const DB_URL= process.env.DB_URL
+const DB_URL =process.env.DB_URL
 
+// Async function to insert data into a table
+async function insertData() {
+  const client = new Client({
+    connectionString:DB_URL
+  });
 
- 
-const client = new Client({
-  connectionString: DB_URL
-})
-
-async function createUsersTable() {
-    await client.connect()
-    const result = await client.query(`
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
-    `)
-    console.log(result)
+  try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username2', 'user3@example.com', 'user_password');";
+    const res = await client.query(insertQuery);
+    console.log('Insertion success:', res); // Output insertion result
+  } catch (err) {
+    console.error('Error during the insertion:', err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
 }
 
-createUsersTable();
+insertData();
